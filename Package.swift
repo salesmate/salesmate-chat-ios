46652424f80bdf11a96fa4977e526364f3ce6d8b
Vishal Nandoriya <vishal.nandoriya@rapidops.com>
2022-08-24 15:26:37 +0530

    Added Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "SalesmateChatSDK",
+    platforms: [
+        .iOS(.v13) //, .macCatalyst(.v15), .macOS(.v10_15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SalesmateChatSDK",
+            targets: ["SalesmateChatSDK"]),
+    ],
+    dependencies: [
+         .package(url: "https://github.com/daltoniam/Starscream", from: "4.0.4"),
+         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.1"),
+         .package(url: "https://github.com/kean/Nuke.git", from: "10.3.1"),
+    ],
+    targets: [
+        .binaryTarget(name: "SalesmateChatSDK",
+                      url: "https://github.com/salesmate/salesmate-chat-ios/releases/download/1.0.0/SalesmateChatSDK.xcframework.zip",
+                      checksum: "a1f3a459ba9b70fefa133e4955d701e68208a05f2d694318a8ad3121251ef2a8"),
+    ]
+)
