303fe9d793c5bdfd238a39f5e305e41037d7bbc1
Vishal Nandoriya <vishal.nandoriya@rapidops.com>
2024-03-11 00:35:40 +0530

    Remove unnecessary code of sample.

--- Examples/Sample-Swift/Sample-Swift/AppDelegate.swift
+++ Examples/Sample-Swift/Sample-Swift/AppDelegate.swift
@@ -20,39 +20,14 @@ var strGlobalDeviceToken:String =  ""
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // Chat configuration
-        if !isValidPercentageString("1.1") {
-            print("Not valid")
-        } else {
-            print("Valid")
-        }
-//        configureSalesmateChatMessengerSDK(env: .production, workspaceID: SALESMATE_CHAT_SDK_WORKSPACE_ID, appKey: SALESMATE_CHAT_SDK_APP_KEY, tenantID: SALESMATE_CHAT_SDK_API_TENANT_ID)
+        configureSalesmateChatMessengerSDK(env: .production, workspaceID: SALESMATE_CHAT_SDK_WORKSPACE_ID, appKey: SALESMATE_CHAT_SDK_APP_KEY, tenantID: SALESMATE_CHAT_SDK_API_TENANT_ID)
         return true
-    }
-
-    func isValidPercentageString(_ string : String) -> Bool {
-        do {
-           let regexPattern = #"^(100(\.00?)?|\d{1,2}(\.\d{1,2})?)$"# // Swift 5.0 and later use #"..."# for raw string literals
-
-           let regex = try NSRegularExpression(pattern: regexPattern, options: [])
-           let range = NSRange(location: 0, length: string.utf16.count)
-           let matches = regex.matches(in: string, options: [], range: range)
-
-           if let match = matches.first {
-               let matchedString = (string as NSString).substring(with: match.range)
-               return true
-           } else {
-               return false
-           }
-        } catch {
-            return false
-        }
     }
     
     func configureSalesmateChatMessengerSDK(env: Environment, workspaceID: String, appKey: String, tenantID: String) {
@@ -62,7 +37,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                    environment: env)
         SalesmateChat.setSalesmateChat(configuration: config)
     }
-
+    
     //MARK: -Remote Notiffications...
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
@@ -75,10 +50,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         SalesmateChat.sendDeviceToken(with: strGlobalDeviceToken)
     }
-
+    
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         print("notification received:\(userInfo)");
-
+        
         if SalesmateChat.isSalesmateChatSDKPushNotification(userInfo: userInfo) {
             SalesmateChat.handlePushNotification(userInfo: userInfo)
             completionHandler(.newData)
@@ -107,6 +82,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return
         }
     }
-
+    
 }
 
